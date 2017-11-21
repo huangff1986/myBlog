d4e4630ba61620bf1a05b0fb26b827bbfbc4a7f6
padapada <906032172@qq.com>
2017-11-21 17:12:10 +0800

    fix server.js

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express')
 
 var app = express()
 
-app.listen(8080)
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+
+app.listen(server_port, server_ip_address);
 
 app.get('/', function(req, res) {
 	res.send("hello world")
